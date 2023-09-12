bc14dfbea69c810e7008bfad4ccf3156bca00612
Martin Rusev <marto260503@abv.bg>
2023-09-12 15:07:05 +0300

    Draw the point on map

--- src/components/MapGermany.vue
+++ src/components/MapGermany.vue
@@ -8,65 +8,33 @@ import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
-import { fromLonLat } from "ol/proj";
+import { fromLonLat } from 'ol/proj';
 import Feature from 'ol/Feature';
 import Point from 'ol/geom/Point';
 import { Vector as VectorLayer } from 'ol/layer';
 import { Vector as VectorSource } from 'ol/source';
-import { Style, Circle, Fill, Stroke } from 'ol/style'; // Import style-related classes
+import { Style, Circle, Fill, Stroke } from 'ol/style'; 
 
 export default {
   props: {
-    journey: Array, 
+    journey: Array,
   },
   mounted() {
     this.initMap();
   },
   methods: {
     initMap() {
+      const vectorSource = new VectorSource();
+      const vectorLayer = new VectorLayer({ source: vectorSource });
+      const coord = fromLonLat([4.7452703, 52.640007]);
       const map = new Map({
+        layers: [new TileLayer({ source: new OSM() })],
+        view: new View({ center: fromLonLat([5, 52]), zoom: 1 }),
         target: 'map',
-        layers: [
-          new TileLayer({
-            source: new OSM(),
-          }),
-        ],
-        view: new View({
-          center: fromLonLat([5, 52]),
-          zoom: 7,
-        }),
       });
-      this.map = map;
-
-      this.visualizePoints(this.journey);
-    },
-    visualizePoints(journey) {
-      const source = new VectorSource();
-      journey.forEach(stop => {
-        const lon = stop.Longitude;
-        const lat = stop.Latitude;
-        const point = new Point(fromLonLat([lon, lat]));
-        const feature = new Feature({
-          geometry: point,
-        });
-
-        const style = new Style({
-          image: new Circle({
-            radius: 6,
-            fill: new Fill({ color: 'red' }),
-            stroke: new Stroke({ color: 'black', width: 2 }),
-          }),
-        });
-
-        feature.setStyle(style);
-        source.addFeature(feature);
-      });
-
-      const vectorLayer = new VectorLayer({
-        source: source,
-      });
-
-      this.map.addLayer(vectorLayer);
+      const feature = new Feature({ geometry: new Point(coord) });
+      vectorSource.addFeature(feature);
+      map.addLayer(vectorLayer);
     },
   },
 };
